078cee802c1603822e4c87c4926a13ad181aa02a
stellanechi <stellanechi65@gmail.com>
2026-01-15 18:03:51 +0100

    updated droid companion

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -8,7 +8,7 @@ import { StyleSheet } from "react-native";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import AboutScreen from "../screens/AboutScreen";
 import SettingsScreen from "../screens/SettingsScreen";
-import OnboardingScreen from "../screens/OnboardingScreen"
+import OnboardingScreen from "../screens/OnboardingScreen";
 import Signup from "../screens/Signup";
 import LoginScreen from "../screens/LoginScreen";
 import BottomTabs from "../screens/BottomTabs";
@@ -41,67 +41,84 @@ import FindSomeoneScreen from "../screens/FindSomeone";
 import ExploreMoreAppsScreen from "../screens/ExploreMoreAppsScreen";
 import CareerDescriptionScreen from "../screens/CareerDescriptionScreen";
 import PartnerWithDroid from "../screens/PartnerWithDroid";
+import DroidCompanion from "../screens/DroidCompanion";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator: React.FC = () => {
-    return (
-        <NavigationContainer>
-            <SafeAreaView style={styles.container}>
-                <Stack.Navigator
-                    initialRouteName="Onboarding"
-                    screenOptions={{
-                        headerShown: false
-                    }}
-                >
-                    <Stack.Screen
-                        name="Onboarding"
-                        component={OnboardingScreen}
-                    />
-                    <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-                    <Stack.Screen name="About" component={AboutScreen} />
-                    <Stack.Screen name="Settings" component={SettingsScreen} />
-                    <Stack.Screen name="Signup" component={Signup} />
-                    <Stack.Screen name="Login" component={LoginScreen} />
-                    <Stack.Screen name="BottomTabs" component={BottomTabs} />
-                    <Stack.Screen name="Notifications" component={NotificationScreen} />
-                    <Stack.Screen name="EventDescription" component={EventDescriptionScreen} />
-                    <Stack.Screen name="InternshipApplication" component={InternshipApplicationScreen} />
-                    <Stack.Screen name="Services" component={MemberServicesScreen} />
-                    <Stack.Screen name="Progression" component={ProgressionScreen} />
-                    <Stack.Screen name="CreateEvent" component={CreateEventScreen} />
-                    <Stack.Screen name="EventDetail" component={EventDetailScreen} />
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <Stack.Navigator
+          initialRouteName="Onboarding"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="About" component={AboutScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="BottomTabs" component={BottomTabs} />
+          <Stack.Screen name="Notifications" component={NotificationScreen} />
+          <Stack.Screen
+            name="EventDescription"
+            component={EventDescriptionScreen}
+          />
+          <Stack.Screen
+            name="InternshipApplication"
+            component={InternshipApplicationScreen}
+          />
+          <Stack.Screen name="Services" component={MemberServicesScreen} />
+          <Stack.Screen name="Progression" component={ProgressionScreen} />
+          <Stack.Screen name="CreateEvent" component={CreateEventScreen} />
+          <Stack.Screen name="EventDetail" component={EventDetailScreen} />
 
-                    {/* Quick Actions - Member Screens */}
-                    <Stack.Screen name="Personal Details" component={PersonalDetailsScreen} />
-                    <Stack.Screen name="Careers" component={CareersScreen} />
-                    <Stack.Screen name="Trainings / Tests" component={TakeTestsScreen} />
-                    <Stack.Screen name="Contact Us" component={ContactUsScreen} />
+          {/* Quick Actions - Member Screens */}
+          <Stack.Screen
+            name="Personal Details"
+            component={PersonalDetailsScreen}
+          />
+          <Stack.Screen name="Careers" component={CareersScreen} />
+          <Stack.Screen name="Trainings / Tests" component={TakeTestsScreen} />
+          <Stack.Screen name="Contact Us" component={ContactUsScreen} />
 
-                    {/* Quick Actions - Staff Screens */}
-                    <Stack.Screen name="Tasks" component={TasksScreen} />
-                    <Stack.Screen name="Payslip" component={PayslipScreen} />
-                    <Stack.Screen name="Training" component={TrainingScreen} />
-                    <Stack.Screen name="Attendance" component={AttendanceScreen} />
-                    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-                    <Stack.Screen name="TAC" component={TermsAndConditions} />
-                    <Stack.Screen name="PAP" component={PrivacyPolicyScreen} />
-                    <Stack.Screen name="AllEventsScreen" component={AllEventsScreen} />
-                    <Stack.Screen name="FindSomeoneScreen" component={FindSomeoneScreen} />
-                    <Stack.Screen name="Explore More Apps" component={ExploreMoreAppsScreen} />
-                    <Stack.Screen name="CareerDescriptionScreen" component={CareerDescriptionScreen} />
-                    <Stack.Screen name="Partner with Us" component={PartnerWithDroid} />
-                </Stack.Navigator>
-            </SafeAreaView>
-        </NavigationContainer>
-    );
+          {/* Quick Actions - Staff Screens */}
+          <Stack.Screen name="Tasks" component={TasksScreen} />
+          <Stack.Screen name="Payslip" component={PayslipScreen} />
+          <Stack.Screen name="Training" component={TrainingScreen} />
+          <Stack.Screen name="Attendance" component={AttendanceScreen} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="TAC" component={TermsAndConditions} />
+          <Stack.Screen name="PAP" component={PrivacyPolicyScreen} />
+          <Stack.Screen name="AllEventsScreen" component={AllEventsScreen} />
+          <Stack.Screen
+            name="FindSomeoneScreen"
+            component={FindSomeoneScreen}
+          />
+          <Stack.Screen
+            name="Explore More Apps"
+            component={ExploreMoreAppsScreen}
+          />
+          <Stack.Screen
+            name="CareerDescriptionScreen"
+            component={CareerDescriptionScreen}
+          />
+          <Stack.Screen name="Partner with Us" component={PartnerWithDroid} />
+          <Stack.Screen name="D'roid Companion" component={DroidCompanion} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
 };
 
 export default RootNavigator;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
 });
