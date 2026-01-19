369033551605a592c5d9f69c0b4e2333a7500f86
stellanechi <stellanechi65@gmail.com>
2026-01-19 12:36:12 +0100

    added upcoming event flyers

--- src/app/constants/Assets.ts
+++ src/app/constants/Assets.ts
@@ -1,11 +1,15 @@
 export const ASSETS = {
-    images: {
-        chessOne: require("../images/png/chess-one.png"),
-        cumsaEvent: require("../images/jpg/cumsaEvent.jpg"),
-        internship: require("../images/jpg/internship.jpg"),
-        CTC2025: require("../images/png/CTC2025.png"),
-        nationalICT: require("../images/png/ICT-ONE.png"),
-        PrimarySchoolRukpokwu: require("../images/jpg/PrimarySchoolRukpokwu.jpeg"),
-        nationalICT2: require("../images/png/ICT-TWO.png"),
-    }
-}
+  images: {
+    chessOne: require("../images/png/chess-one.png"),
+    cumsaEvent: require("../images/jpg/cumsaEvent.jpg"),
+    internship: require("../images/jpg/internship.jpg"),
+    CTC2025: require("../images/png/CTC2025.png"),
+    nationalICT: require("../images/png/ICT-ONE.png"),
+    PrimarySchoolRukpokwu: require("../images/jpg/PrimarySchoolRukpokwu.jpeg"),
+    nationalICT2: require("../images/png/ICT-TWO.png"),
+    aiForDevelopers: require("../images/png/AI-for-Developers.png"),
+    liftOffConference: require("../images/png/Lift-Off-Conference.png"),
+    makaTonBootcamp: require("../images/png/MakaTon-Bootcamp.png"),
+    techNormads: require("../images/png/Tech-Normads.png"),
+  },
+};
