80e3b3e77974f8ca81f71fa669170ff056c88dd6
stellanechi <stellanechi65@gmail.com>
2026-02-19 16:36:38 +0100

    added droidevent slice to reduce store and implemented on the eventdescription and eventprogress screen

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -7,21 +7,23 @@ import { membershipTierSlice } from "./slice/membershiptierslice";
 import { eventSlice } from "./slice/eventSlice";
 import { affiliatedAppsSlice } from "./slice/affiliatedAppsSlice";
 import { internshipSlice } from "./slice/internshipSlice";
+import droideventsReducer from "./slice/droideventsSlice"; // ADDED THIS
 
 const rootReducer = combineReducers({
-    // Add all your slice reducers here
-    user: userSlice.reducer,
-    location: locationSlice.reducer,
-    notifications: notificationsSlice.reducer,
-    membershipTier: membershipTierSlice.reducer,
-    events: eventSlice.reducer,
-    affiliatedApps: affiliatedAppsSlice.reducer,
-    internship: internshipSlice.reducer
+  // Add all your slice reducers here
+  user: userSlice.reducer,
+  location: locationSlice.reducer,
+  notifications: notificationsSlice.reducer,
+  membershipTier: membershipTierSlice.reducer,
+  events: eventSlice.reducer,
+  droidevents: droideventsReducer, // ADDED THIS
+  affiliatedApps: affiliatedAppsSlice.reducer,
+  internship: internshipSlice.reducer,
 });
 
 // 1. Configure the store using the root reducer directly.
 export const store = configureStore({
-    reducer: rootReducer,
+  reducer: rootReducer,
 });
 
 // 2. Persistence objects (persistor) are removed.
